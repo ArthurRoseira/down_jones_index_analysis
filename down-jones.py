c391cf9917841f8345fdb8d44aa7ee18bc0485c9
ArthurRoseira <arthurroseira95@gmail.com>
2021-02-01 17:08:05 -0300

    Stationary transformation testing

--- down-jones.py
+++ down-jones.py
@@ -7,7 +7,9 @@ import statsmodels.api as sm
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from statsmodels.tsa.stattools import adfuller, kpss
 from pandas.plotting import autocorrelation_plot
+from statsmodels.tsa.seasonal import seasonal_decompose
 import math
+from scipy.stats import boxcox
 
 
 df = pd.read_csv('DJI.csv')
@@ -23,7 +25,7 @@ print(w_moving_Avg)
 w_moving_Avg.plot()
 m_moving_Avg.plot()
 df['Close'].plot()
-plt.title('Noise Rduction with Aggregation')
+plt.title('Noise Reduction with Aggregation')
 plt.legend(['weekly', 'monthly', 'Original'])
 plt.show()
 
@@ -48,8 +50,69 @@ plot_acf(df['Close'], ax=ax1,
          title='ACF Plot StatsModels Original', unbiased=True)
 ax2.set_title('ACF Plot Plt Original')
 autocorrelation_plot(df['Close'], ax=ax2)
-plot_acf(f_diff, ax=ax3, lags=10, unbiased=True,
+plot_acf(f_diff, ax=ax3, unbiased=True,
          title='ACF Plot StatsModels Diff 1')
 ax4.set_title('ACF Plot Plt Diff 1')
 autocorrelation_plot(f_diff, ax=ax4)
+plt.show()
+
+### Seazonality Verification #######
+# Weekly AutoCorrelation
+plot_acf(df['Close'], lags=7,
+         title='ACF Plot StatsModels Original', unbiased=True)
+season = seasonal_decompose(df['Close'], period=7)
+fig = season.plot()
+fig.set_size_inches(16, 8)
+plt.show()
+
+#### Stationarity Test ######
+#### acf plot ####
+plot_acf(f_diff, title='First Differentiation', unbiased=True)
+plt.show()
+adfinput = adfuller(f_diff, autolag='AIC')
+adftest = pd.Series(adfinput[0:4], index=['Teste Estatistico Dickey Fuller',
+                                          'Valor-P', 'Lags Usados', 'Número de observações usadas'])
+adftest = round(adftest, 4)
+for key, value in adfinput[4].items():
+    adftest["Valores Críticos (%s)" % key] = value.round(4)
+print(adftest)
+# KPSS Test
+kpss_input = kpss(f_diff, regression='c', nlags="auto")
+kpss_test = pd.Series(kpss_input[0:3], index=[
+                      'Teste Statistico KPSS', 'Valor-P', 'Lags Usados'])
+kpss_test = round(kpss_test, 4)
+for key, value in kpss_input[3].items():
+    kpss_test["Valores Críticos (%s)" % key] = value
+print(kpss_test)
+season = seasonal_decompose(f_diff, period=30)
+fig = season.plot()
+fig.set_size_inches(16, 8)
+plt.show()
+# The p-value is obtained is greater than significance level of 0.05 and the ADF statistic is higher than any of the critical values.
+# Kpps is stationary
+
+#### Applying Box Cox  Transformation in  Data #########
+# Data must be positive
+boxcox_transform = boxcox(df['Close'], -0.5)
+#boxcox_transform = pd.Series(boxcox_transform).diff().dropna()
+plot_acf(boxcox_transform, title='First Differentiation', unbiased=True)
+plt.show()
+adfinput = adfuller(boxcox_transform, autolag='AIC')
+adftest = pd.Series(adfinput[0:4], index=['Teste Estatistico Dickey Fuller',
+                                          'Valor-P', 'Lags Usados', 'Número de observações usadas'])
+adftest = round(adftest, 4)
+for key, value in adfinput[4].items():
+    adftest["Valores Críticos (%s)" % key] = value.round(4)
+print(adftest)
+# KPSS Test
+kpss_input = kpss(boxcox_transform, regression='c', nlags="auto")
+kpss_test = pd.Series(kpss_input[0:3], index=[
+                      'Teste Statistico KPSS', 'Valor-P', 'Lags Usados'])
+kpss_test = round(kpss_test, 4)
+for key, value in kpss_input[3].items():
+    kpss_test["Valores Críticos (%s)" % key] = value
+print(kpss_test)
+season = seasonal_decompose(boxcox_transform, period=30)
+fig = season.plot()
+fig.set_size_inches(16, 8)
 plt.show()
